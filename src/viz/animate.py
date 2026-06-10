2f2594b2cdd9e50e9845f8c803afe4e098906610
Kasidit Boonpornpichet <acc.newisreal@gmail.com>
2026-06-10 14:19:19 +0700

    viz: AnimRotationsPlot + inheritance refactor

--- src/viz/animate.py
+++ src/viz/animate.py
@@ -1,19 +1,20 @@
 import numpy as np
 import plotly.graph_objects as go
+from scipy.spatial.transform import Rotation as R
 
-from src.model import get_alex_bind_model
+from src.model import get_alex_bind_model, get_paper_plane_mesh
 from src.utils import extract_frame, pack_frame_to_matrix
-from src.viz.static import DEFAULT_SKELETON_PLOT_LAYOUT
+from src.viz.static import DEFAULT_ROTATION_PLOT_LAYOUT, DEFAULT_SKELETON_PLOT_LAYOUT
 
 
-class AnimatedSkeletonsPlotter:
+class BaseAnimatedPlotter:
     """
-    Plotter wrapper for animated skeleton plots
+    Base class for common animated plotter logics
     """
 
     def __init__(self, fps=30, stride=2, **fig_layout):
         self.fig = go.Figure(layout=fig_layout)
-        self.bind_model = get_alex_bind_model()
+        self.static_traces = []
         self.frames_traces = []    # frames = [[trace, ...], ...]
         self.fps = fps
         self.stride = stride
@@ -28,88 +29,29 @@ class AnimatedSkeletonsPlotter:
             # "easing": "linear"
         }
 
-
-    def _build_bones(self, pos_dict):
-        """
-        Build bones for lines plotting
-        """
-        bones = []
-        parent_name = list(self.bind_model.keys())[0]
-        stack = [(parent_name, self.bind_model[parent_name])]
-
-        while stack:
-            parent_name, data = stack.pop()
-            for child_name in data["children"]:
-                bones.append(pos_dict[parent_name])
-                bones.append(pos_dict[child_name])
-                bones.append(np.array([np.nan, np.nan, np.nan]))
-                stack.append((child_name, data["children"][child_name]))
-
-        return np.array(bones)
-
-
-    def add_skeleton_frames(
-        self, pos_dict,
-        joints_size=5, bones_width=10,
-        offset=None, name=None, joints_color=None, bones_color=None
-        ):
-        first_key = list(pos_dict.keys())[0]
-        n_frames = pos_dict[first_key].shape[0]
-
-        for _ in range(0, n_frames - len(self.frames_traces)):
-            self.frames_traces.append([])
-
-        for f in range(n_frames):
-            f_pos_dict = extract_frame(pos_dict, f)      
-            f_points = pack_frame_to_matrix(f_pos_dict)
-            f_bones = self._build_bones(f_pos_dict)
-
-            if offset is not None:
-                offset = np.array(offset)
-                f_points += offset
-                f_bones  += offset
-
-            f_joints_trace = go.Scatter3d(
-                x=f_points[:, 0], y=f_points[:, 2], z=f_points[:, 1],
-                marker=dict(size=joints_size, color=joints_color),
-                mode="markers",
-                text=list(pos_dict.keys()),
-                name=f"{name} (joints)",
-                showlegend=name is not None,
-            )
-
-            f_bones_trace = go.Scatter3d(
-                x=f_bones[:, 0], y=f_bones[:, 2], z=f_bones[:, 1],
-                line=dict(width=bones_width, color=bones_color),
-                mode="lines",
-                connectgaps=False,
-                name=f"{name} (bones)",
-                showlegend=name is not None,
-                hoverinfo="skip",
-            )
-
-            self.frames_traces[f].append(f_joints_trace)
-            self.frames_traces[f].append(f_bones_trace)
-
-        return self
-
-
+    
     def update_layout(self, layout_dict):
         self.fig.update_layout(layout_dict)
         return self
 
 
-    def apply_defualt_layout(self):
-        self.fig.update_layout(DEFAULT_SKELETON_PLOT_LAYOUT)
-        return self
-
-
     def _build_fig(self):
-        self.fig.add_traces(data=self.frames_traces[0])
-        self.fig.frames = [go.Frame(
-            data=[trace for trace in self.frames_traces[f]],
-            name=str(f),
-        ) for f in range(0, len(self.frames_traces), self.stride)]
+        if len(self.static_traces) > 0:
+            self.fig.add_traces(data=self.static_traces)
+
+
+        if len(self.frames_traces) > 0:
+            self.fig.add_traces(data=self.frames_traces[0])
+
+            animated_start = len(self.static_traces)
+            animated_count = len(self.frames_traces[0])
+            frame_trace_indices = list(range(animated_start, animated_start + animated_count))
+
+            self.fig.frames = [go.Frame(
+                data=[trace for trace in self.frames_traces[f]],
+                traces=frame_trace_indices,
+                name=str(f),
+            ) for f in range(0, len(self.frames_traces), self.stride)]
 
 
     def _build_buttons(self):
@@ -200,3 +142,176 @@ class AnimatedSkeletonsPlotter:
             self.build()
         self.fig.show()
         # return self
+
+
+class AnimatedSkeletonsPlotter(BaseAnimatedPlotter):
+    """
+    Plotter wrapper for animated skeleton plots
+    """
+
+    def __init__(self, fps=30, stride=2, **fig_layout):
+        super().__init__(fps=fps, stride=stride, **fig_layout)
+        self.bind_model = get_alex_bind_model()
+
+
+    def _build_bones(self, pos_dict):
+        """
+        Build bones for lines plotting
+        """
+        bones = []
+        parent_name = list(self.bind_model.keys())[0]
+        stack = [(parent_name, self.bind_model[parent_name])]
+
+        while stack:
+            parent_name, data = stack.pop()
+            for child_name in data["children"]:
+                bones.append(pos_dict[parent_name])
+                bones.append(pos_dict[child_name])
+                bones.append(np.array([np.nan, np.nan, np.nan]))
+                stack.append((child_name, data["children"][child_name]))
+
+        return np.array(bones)
+
+
+    def add_skeleton_frames(
+        self, pos_dict,
+        joints_size=5, bones_width=10,
+        offset=None, name=None, joints_color=None, bones_color=None
+        ):
+        first_key = list(pos_dict.keys())[0]
+        n_frames = pos_dict[first_key].shape[0]
+
+        for _ in range(0, n_frames - len(self.frames_traces)):
+            self.frames_traces.append([])
+
+        for f in range(n_frames):
+            f_pos_dict = extract_frame(pos_dict, f)      
+            f_points = pack_frame_to_matrix(f_pos_dict)
+            f_bones = self._build_bones(f_pos_dict)
+
+            if offset is not None:
+                offset = np.array(offset)
+                f_points += offset
+                f_bones  += offset
+
+            f_joints_trace = go.Scatter3d(
+                x=f_points[:, 0], y=f_points[:, 2], z=f_points[:, 1],
+                marker=dict(size=joints_size, color=joints_color),
+                mode="markers",
+                text=list(pos_dict.keys()),
+                name=f"{name} (joints)",
+                showlegend=name is not None,
+            )
+
+            f_bones_trace = go.Scatter3d(
+                x=f_bones[:, 0], y=f_bones[:, 2], z=f_bones[:, 1],
+                line=dict(width=bones_width, color=bones_color),
+                mode="lines",
+                connectgaps=False,
+                name=f"{name} (bones)",
+                showlegend=name is not None,
+                hoverinfo="skip",
+            )
+
+            self.frames_traces[f].append(f_joints_trace)
+            self.frames_traces[f].append(f_bones_trace)
+
+        return self
+
+
+    def apply_defualt_layout(self):
+        self.fig.update_layout(DEFAULT_SKELETON_PLOT_LAYOUT)
+        return self
+
+
+class AnimatedRotationsPlotter(BaseAnimatedPlotter):
+    """
+    Plotter wrapper for animated rotation plots
+    """
+
+    def __init__(self, fps=30, stride=2, **fig_layout):
+        super().__init__(fps=fps, stride=stride, **fig_layout)
+        self.model_vertices, self.model_faces = get_paper_plane_mesh(recenter=True, scale=0.25)
+        self.model_vertices = R.from_euler(('xz'), (-90,-90), degrees=True).apply(self.model_vertices)
+        self.ref_vector = np.array([[-1, 0, 0], [1, 0, 0]])
+        # self.add_quat([0, 0, 0, 1], color="tan", model_opacity=ref_model_opcaity, name="ref")
+        self._build_sphere()
+
+
+    def _build_sphere(self, resolution=25):
+        theta = np.linspace(0, 2 * np.pi, resolution)
+        phi   = np.linspace(0, np.pi, resolution)
+        theta_grid, phi_grid = np.meshgrid(theta, phi)
+
+        x = np.sin(phi_grid) * np.cos(theta_grid)
+        y = np.sin(phi_grid) * np.sin(theta_grid)
+        z = np.cos(phi_grid)
+
+        self.static_traces.append(go.Surface(
+            x=x, y=y, z=z,
+            colorscale=[(0, "#4a90d9"), (1, "#4a90d9")],
+            opacity=0.0,
+            showscale=False,
+            contours=dict(
+                x=dict(show=True, color="grey", width=1),
+                y=dict(show=True, color="grey", width=1),
+                z=dict(show=True, color="grey", width=1)
+            ),
+            hoverinfo="skip"
+        ))
+
+
+    def add_quat_frames(self, quats, color=None, model_opacity=1, name=None):
+        n_frames = np.array(quats).shape[0]
+
+        for _ in range(0, n_frames - len(self.frames_traces)):
+            self.frames_traces.append([])
+
+        for f, quat in enumerate(quats):
+            f_rotation = R.from_quat(quat)
+            f_rotated_vertices = f_rotation.apply(self.model_vertices)
+            f_rotated_axis = f_rotation.apply(self.ref_vector)
+
+            f_model = go.Mesh3d(
+                x=f_rotated_vertices[:, 0],
+                y=f_rotated_vertices[:, 1],
+                z=f_rotated_vertices[:, 2],
+                i=self.model_faces[:, 0],
+                j=self.model_faces[:, 1],
+                k=self.model_faces[:, 2],
+                color=color,
+                opacity=model_opacity,
+                showlegend=False,
+            )
+
+            f_axis = go.Scatter3d(
+                x=f_rotated_axis[:, 0],
+                y=f_rotated_axis[:, 1],
+                z=f_rotated_axis[:, 2],
+                mode="lines",
+                line=dict(color=color, width=6),
+                name=name,
+                showlegend=name is not None,
+            )
+
+            f_axis_head = go.Scatter3d(
+                x=[f_rotated_axis[1, 0]],
+                y=[f_rotated_axis[1, 1]],
+                z=[f_rotated_axis[1, 2]],
+                mode="markers",
+                marker=dict(color=color, size=6),
+                showlegend=False,
+            )
+
+            self.frames_traces[f].append(f_model)
+            self.frames_traces[f].append(f_axis)
+            self.frames_traces[f].append(f_axis_head)
+
+        return self
+
+
+    def apply_defualt_layout(self):
+        self.fig.update_layout(DEFAULT_ROTATION_PLOT_LAYOUT)
+        return self
+
+
--- src/viz/static.py
+++ src/viz/static.py
@@ -33,13 +33,102 @@ DEFAULT_SKELETON_PLOT_LAYOUT = dict(
 )
 
 
-class StaticRotationsPlotter:
+class BaseStaticPlotter:
+    """
+    Base class for common static plotter logics
+    """
+
+    def __init__(self, **fig_layout):
+        self.fig = go.Figure(layout=fig_layout)
+
+        
+    def update_layout(self, layout_dict):
+        self.fig.update_layout(layout_dict)
+        return self
+
+
+    def show(self):
+        self.fig.show()
+        # return self
+
+
+class StaticSkeletonsPlotter(BaseStaticPlotter):
+    """
+    Plotter wrapper for static skeleton plots
+    """
+
+    def __init__(self, **fig_layout):
+        super().__init__(**fig_layout)
+        self.bind_model = get_alex_bind_model()
+
+
+    def _build_bones(self, pos_dict):
+        """
+        Build bones for lines plotting
+        """
+        bones = []
+        parent_name = list(self.bind_model.keys())[0]
+        stack = [(parent_name, self.bind_model[parent_name])]
+
+        while stack:
+            parent_name, data = stack.pop()
+            for child_name in data["children"]:
+                bones.append(pos_dict[parent_name])
+                bones.append(pos_dict[child_name])
+                bones.append(np.array([np.nan, np.nan, np.nan]))
+                stack.append((child_name, data["children"][child_name]))
+
+        return np.array(bones)
+
+
+    def add_skeleton(
+        self, pos_dict, 
+        joints_size=5, bones_width=10,
+        offset=None, name=None, joints_color=None, bones_color=None
+        ):
+        points = pack_frame_to_matrix(pos_dict)
+        bones = self._build_bones(pos_dict)
+
+        if offset is not None:
+            offset = np.array(offset)
+            points += offset
+            bones  += offset
+
+        self.fig.add_trace(go.Scatter3d(
+            x=points[:, 0], y=points[:, 2], z=points[:, 1],
+            marker=dict(size=joints_size, color=joints_color),
+            mode="markers",
+            text=list(pos_dict.keys()),
+            name=f"{name} (joints)",
+            showlegend=name is not None,
+        ))
+
+
+        self.fig.add_trace(go.Scatter3d(
+            x=bones[:, 0], y=bones[:, 2], z=bones[:, 1],
+            line=dict(width=bones_width, color=bones_color),
+            mode="lines",
+            connectgaps=False,
+            name=f"{name} (bones)",
+            showlegend=name is not None,
+            hoverinfo="skip",
+        ))
+
+        return self
+
+
+    def apply_defualt_layout(self):
+        self.fig.update_layout(DEFAULT_SKELETON_PLOT_LAYOUT)
+        return self
+
+
+class StaticRotationsPlotter(BaseStaticPlotter):
     """
     Plotter wrapper for static rotation plots
     """
 
     def __init__(self, ref_model_opcaity=0.2, **fig_layout):
-        self.fig = go.Figure(layout=fig_layout)
+        super().__init__(**fig_layout)
         self.model_vertices, self.model_faces = get_paper_plane_mesh(recenter=True, scale=0.25)
         self.model_vertices = R.from_euler(('xz'), (-90,-90), degrees=True).apply(self.model_vertices)
         self.ref_vector = np.array([[-1, 0, 0], [1, 0, 0]])
@@ -107,96 +196,7 @@ class StaticRotationsPlotter:
         ))
 
 
-    def update_layout(self, layout_dict):
-        self.fig.update_layout(layout_dict)
-        return self
-
-
     def apply_defualt_layout(self):
         self.fig.update_layout(DEFAULT_ROTATION_PLOT_LAYOUT)
         return self
 
-
-    def show(self):
-        self.fig.show()
-        # return self
-
-
-class StaticSkeletonsPlotter:
-    """
-    Plotter wrapper for static skeleton plots
-    """
-
-    def __init__(self, **fig_layout):
-        self.fig = go.Figure(layout=fig_layout)
-        self.bind_model = get_alex_bind_model()
-
-
-    def _build_bones(self, pos_dict):
-        """
-        Build bones for lines plotting
-        """
-        bones = []
-        parent_name = list(self.bind_model.keys())[0]
-        stack = [(parent_name, self.bind_model[parent_name])]
-
-        while stack:
-            parent_name, data = stack.pop()
-            for child_name in data["children"]:
-                bones.append(pos_dict[parent_name])
-                bones.append(pos_dict[child_name])
-                bones.append(np.array([np.nan, np.nan, np.nan]))
-                stack.append((child_name, data["children"][child_name]))
-
-        return np.array(bones)
-
-
-    def add_skeleton(
-        self, pos_dict, 
-        joints_size=5, bones_width=10,
-        offset=None, name=None, joints_color=None, bones_color=None
-        ):
-        points = pack_frame_to_matrix(pos_dict)
-        bones = self._build_bones(pos_dict)
-
-        if offset is not None:
-            offset = np.array(offset)
-            points += offset
-            bones  += offset
-
-        self.fig.add_trace(go.Scatter3d(
-            x=points[:, 0], y=points[:, 2], z=points[:, 1],
-            marker=dict(size=joints_size, color=joints_color),
-            mode="markers",
-            text=list(pos_dict.keys()),
-            name=f"{name} (joints)",
-            showlegend=name is not None,
-        ))
-
-
-        self.fig.add_trace(go.Scatter3d(
-            x=bones[:, 0], y=bones[:, 2], z=bones[:, 1],
-            line=dict(width=bones_width, color=bones_color),
-            mode="lines",
-            connectgaps=False,
-            name=f"{name} (bones)",
-            showlegend=name is not None,
-            hoverinfo="skip",
-        ))
-
-        return self
-
-
-    def update_layout(self, layout_dict):
-        self.fig.update_layout(layout_dict)
-        return self
-
-
-    def apply_defualt_layout(self):
-        self.fig.update_layout(DEFAULT_SKELETON_PLOT_LAYOUT)
-        return self
-
-
-    def show(self):
-        self.fig.show()
-        # return self
